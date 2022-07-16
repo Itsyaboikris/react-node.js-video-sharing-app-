3017a8f4895e838c80edd53769c4b207eaccd8f6
itsyaboikris <kristoff_riley@outlook.com>
2022-07-16 18:45:19 -0400

    initial routing setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
+
 const PORT = process.env.PORT || 5000;
 const app = express();
 dotenv.config();
@@ -13,6 +17,10 @@ const connect = () => {
 		}
 	).catch(err => {throw err})
 }
+
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comment", commentRoutes);
 
 app.listen(PORT, () => {
 	connect();
