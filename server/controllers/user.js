ffc9cb4f82e620f720cd569e51e072dde8548c2e
itsyaboikris <kristoff_riley@outlook.com>
2022-07-17 05:32:29 -0400

    update user endpoint

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,22 @@
-export const updateUser = (req, res, next) => {
-	
+import { createError } from "../middleware/error.js"
+import User from "../models/User.js"
+
+export const updateUser = async (req, res, next) => {
+	if(req.params.id === req.user.id) {
+		try {
+			const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+				$set: req.body
+			},{
+				new: true
+			});
+
+			res.status(200).json(updatedUser)
+		} catch (error) {
+			next(err)
+		}
+	} else {
+		return next(createError("403", "You can only update you account"))
+	}
 }
 
 export const deleteUser = (req, res, next) => {
