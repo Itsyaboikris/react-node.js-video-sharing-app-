bf162fbc196f4bf5867fa12cacfce59196507eab
itsyaboikris <kristoff_riley@outlook.com>
2022-07-17 05:08:46 -0400

    user routes setup

---
+++ server/controllers/user.js
@@ -0,0 +1,28 @@
+export const updateUser = (req, res, next) => {
+	
+}
+
+export const deleteUser = (req, res, next) => {
+	
+}
+
+export const getUser = (req, res, next) => {
+	
+}
+
+export const subscribe = (req, res, next) => {
+	
+}
+
+
+export const unsubscribe = (req, res, next) => {
+	
+}
+
+export const likeVideo = (req, res, next) => {
+	
+}
+
+export const dislikeVideo = (req, res, next) => {
+	
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,27 @@
-import express, { Router } from "express";
+import express from "express";
+import { updateUser, deleteUser, getUser, subscribe, likeVideo } from "../controllers/user.js";
 
-const router = express.Router()
+const router = express.Router();
 
+//update user
+router.put("/:id", updateUser)
 
+//delete user
+router.delete("/:id", deleteUser)
+
+//get a user
+router.get("/find/:id", getUser)
+
+//subscribe a user
+router.put("/sub/:id", subscribe)
+
+//unsubscribe a user
+router.put("/unsub/:id", subscribe)
+
+//like a video
+router.put("/like/:videoId", likeVideo)
+
+//dislike a video
+router.put("/dislike/:videoId", likeVideo)
 
 export default router;
