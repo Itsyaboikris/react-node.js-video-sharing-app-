a2c5bb3b8ea5baebcccffae60f26c86b4a8d10d3
itsyaboikris <kristoff_riley@outlook.com>
2022-07-17 16:09:54 -0400

    get user endpoint

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -34,7 +34,12 @@ export const deleteUser = async (req, res, next) => {
 }
 
 export const getUser = async (req, res, next) => {
-	
+	try {
+		const user = await User.findById(req.params.id)
+		res.status(200).json(user);
+	} catch (err) {
+		next(err)
+	}
 }
 
 export const subscribe = async (req, res, next) => {
