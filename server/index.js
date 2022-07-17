759deb9cbbaa0ab5af88971ea4a52e28eaabc5c8
itsyaboikris <kristoff_riley@outlook.com>
2022-07-17 04:57:21 -0400

    sign in endpoint

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/users.js";
 import videoRoutes from "./routes/videos.js";
@@ -20,6 +21,7 @@ const connect = () => {
 }
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videoRoutes);
