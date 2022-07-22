ce878c4b73fea1d00655f7262e344abe5c2d6a65
itsyaboikris <kristoff_riley@outlook.com>
2022-07-21 21:50:06 -0400

    comment endpoints

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,7 +1,16 @@
-import express, { Router } from "express";
+import express from "express";
+import { addComment, deleteComment, getComments } from "../controllers/comment.js";
+import {verifyToken} from "../middleware/verifyToken.js"
 
 const router = express.Router()
 
+// add coment 
+router.post("/", verifyToken, addComment)
 
+// delete comment
+router.delete("/:id", verifyToken, deleteComment)
+
+//get comment
+router.get("/:videoId", getComments)
 
 export default router;
