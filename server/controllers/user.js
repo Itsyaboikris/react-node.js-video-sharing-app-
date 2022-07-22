d1a8f3b12f86a4732e03ac49eca7764e2140baf7
itsyaboikris <kristoff_riley@outlook.com>
2022-07-21 20:33:43 -0400

    fixed error
    fixed error in subscribe and unsubscribed options

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -49,25 +49,22 @@ export const getUser = async (req, res, next) => {
 
 export const subscribe = async (req, res, next) => {
 	try {
-		await User.findById(req.user.id,{
-			$push:{subscribedUsers: req.params.id},
-		});
-
-		await User.findByIdAndUpdate(req.params.id,{
-			$inc:{subscribers: 1},
-		});
-
-		res.status(200).json("Subscription successful.")
-
+	  	await User.findByIdAndUpdate(req.user.id, {
+			$push: { subscribedUsers: req.params.id },
+	  	});
+	  	await User.findByIdAndUpdate(req.params.id, {
+			$inc: { subscribers: 1 },
+	  	});
+	  	res.status(200).json("Subscription successfull.")
 	} catch (err) {
-		next(err)
+	  next(err);
 	}
-}
+};
 
 
 export const unsubscribe = async (req, res, next) => {
 	try {
-		await User.findById(req.user.id,{
+		await User.findByIdAndUpdate(req.user.id,{
 			$pull:{subscribedUsers: req.params.id}
 		});
 
